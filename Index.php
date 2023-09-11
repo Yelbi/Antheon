fe72314a7677cf3c2cd0a18c9994c586fa1ce45e
Yelbi <bgrandiel@gmail.com>
2023-09-11 13:34:53 -0400

    hola

--- Index.php
+++ Index.php
@@ -9,15 +9,15 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="http://localhost/Antheon/Styles/Index_style.css">
-    <link rel="stylesheet" href="http://localhost/Antheon/Styles/Index_Index_style.css">
+    <link rel="stylesheet" href="/Styles/Index_style.css">
+    <link rel="stylesheet" href="/Styles/Index_Index_style.css">
     <title>Anthesis</title>
 </head>
 <body>
 <nav class="menu">
         <div>
             <a href="/Index.html">
-                <img src="http://localhost/Antheon/img/floral-logo-a-02-412-2048x1366.png" alt="">
+                <img src="/img/floral-logo-a-02-412-2048x1366.png" alt="">
             </a>
         </div>
         <ul>
@@ -34,38 +34,21 @@
                 <a href="/Flores Peligrosas.html">Flores Peligrosas</a>
             </li>
         </ul>
-    </nav>
-    </div>
+    <img src="/img/" alt="">
+</nav>
     <main>
         <div class="flex-container">
             <div class="flex-slide home">
                 <div class="flex-title flex-title-home">Primavera</div>
-                <div class="flex-about flex-about-home"><p>Click here to navigate to the home section of the website</p></div>
             </div>
             <div class="flex-slide about">
                 <div class="flex-title">Verano</div>
-                <div class="flex-about"><p>Click here to navigate to the About section of the website</p></div>
             </div>
             <div class="flex-slide work">
                 <div class="flex-title">Otoño</div>
-                <div class="flex-about"><p>Listing relevant snippets of work:</p>
-                    <ul>
-                        <li>First piece of work</li>
-                        <li>Second piece of work</li>
-                        <li>Third piece of work</li>
-                    </ul>
                 </div>
-            </div>
             <div class="flex-slide contact">
                 <div class="flex-title">Invierno</div>
-                        <div class="flex-about">
-                            <p>Use the contact form below</p>
-                            <form class="contact-form">
-                                <p>Email <input type="text" name="email"></p>
-                                <p>Comment <textarea type="text" name="comment" row="5"></textarea></p>
-                                <p><input type="submit" name="email" value="Send Message"></p>
-                            </form>
-        
                 </div>
             </div>
         </div>
