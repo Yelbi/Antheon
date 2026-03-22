275a046f41a85e35e9246553f8e8fc386c411e35
Grandiel <bgrandiel@gmail.com>
2026-03-22 16:15:39 -0400

    22/3/26

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import path from "path";
 
 const nextConfig: NextConfig = {
   outputFileTracingRoot: path.join(__dirname),
+  serverExternalPackages: ["pg", "@prisma/client", "@prisma/adapter-pg"],
   images: {
     imageSizes: [256, 384, 480],
     deviceSizes: [640, 828, 1080, 1200, 1920],
